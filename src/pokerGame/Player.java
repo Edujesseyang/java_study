e063c73e6ab8a76b214b08b0cb44f00e3a3a76fa
Edujesseyang <edujesseyang@gmail.com>
2024-08-28 11:52:46 -0700

    add

---
+++ src/pokerGame/Player.java
@@ -0,0 +1,30 @@
+package src.pokerGame;
+
+import java.util.ArrayList;
+
+
+public class Player {
+    private String name;
+    private ArrayList<String> handCards;
+
+    public Player() {
+    }
+
+    public Player(ArrayList<String> handCards, String name) {
+        this.handCards = handCards;
+        this.name = name;
+    }
+
+    public void pickCard(String newCard) {
+        this.handCards.add(newCard);
+    }
+
+    public ArrayList<String> getHandCards() {
+        return handCards;
+    }
+
+    public void throwCard(String cards) {
+        this.handCards.remove(cards);
+    }
+
+}
