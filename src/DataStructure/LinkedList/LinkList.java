8f8defb783f8d6d0a4c0396f68d419110f801cf2
Edujesseyang <edujesseyang@gmail.com>
2025-04-09 15:43:46 -0700

    add
    add

--- src/DataStructure/LinkedList/LinkList.java
+++ src/DataStructure/LinkedList/LinkList.java
@@ -1,5 +1,7 @@
 package src.DataStructure.LinkedList;
 
+import java.lang.reflect.Array;
+
 public class LinkList<T> {
     private static class Node<T> {
         private T value;
@@ -31,10 +33,13 @@ public class LinkList<T> {
     }
 
     private Node<T> head;
+    private Node<T> tail;
     private int length = 0;
+    private final Class<T> clazz;
 
-    LinkList() {
-        head = null;
+    LinkList(Class<T> clazz) {
+        this.clazz = clazz;
+        this.head = null;
     }
 
     int getLength() {
@@ -45,18 +50,22 @@ public class LinkList<T> {
         Node<T> newNode = new Node<>(value);
         if (head == null) {
             head = newNode;
-            this.length++;
+            length = 1;
+            tail = head;
             return;
         }
-        Node<T> current = this.head;
-        while (current.next != null) {
-            current = current.next;
-        }
-        newNode.setIndex(this.length);
-        current.next = newNode;
+
+        tail.next = newNode;
+        tail = newNode;
+        newNode.setIndex(length);
+        length++;
     }
 
-    boolean delete(int val) {
+    boolean delete(T val) {
+        if (head == null) {
+            return false;
+        }
+
         if (head.getValue().equals(val)) {
             this.head = head.next;
             this.length--;
@@ -64,10 +73,14 @@ public class LinkList<T> {
         }
 
         Node<T> current = head;
-        while (current != null) {
+        while (current.next != null) {
             if (current.next.getValue().equals(val)) {
+                if (current.next == tail) {
+                    tail = current;
+                }
                 current.next = current.next.next;
                 this.length--;
+                reIndexing(current.getIndex());
                 return true;
             }
             current = current.next;
@@ -75,6 +88,81 @@ public class LinkList<T> {
         return false;
     }
 
+    void insert(int index, T val) {
+        if (index < 0 || index > length) {
+            throw new IndexOutOfBoundsException("Out of bound");
+        }
+
+        Node<T> newNode = new Node<>(val);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+
+        if (tail == null || newNode.next == null) {
+            tail = newNode;
+        }
+
+        length++;
+        reIndexing(index);
+    }
+
+    T get(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("out of bound");
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.getValue();
+    }
+
+    void removeAt(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Out of bound");
+        }
+
+        if (index == 0) {
+            head = head.next;
+            if (head == null) {
+                tail = null;
+            }
+        } else {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+
+            Node<T> nodeToRemove = current.next;
+            current.next = nodeToRemove.next;
+
+            if (nodeToRemove == tail) {
+                tail = current;
+            }
+        }
+        length--;
+        reIndexing(index);
+    }
+
+    int indexOf(T val) {
+        Node<T> current = head;
+        while (current != null) {
+            if (current.getValue().equals(val)) {
+                return current.getIndex();
+            }
+            current = current.next;
+        }
+        return -1;
+    }
 
     void print() {
         Node<T> current = head;
@@ -84,5 +172,42 @@ public class LinkList<T> {
             current = current.next;
         }
         System.out.println("}");
+    }
+
+    void printIndex() {
+        Node<T> current = head;
+        System.out.print("[");
+        while (current != null) {
+            System.out.print("(" + current.getIndex() + "): " + current.getValue() + ", ");
+            current = current.next;
+        }
+        System.out.println("]");
+    }
+
+    void reIndexing(int start) {
+        if (start < 0 || start >= this.length) {
+            throw new IndexOutOfBoundsException("Out of bond.");
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < start; i++) {
+            current = current.next;
+        }
+
+        while (current != null) {
+            current.setIndex(start++);
+            current = current.next;
+        }
+    }
+
+    T[] toArray() {
+        @SuppressWarnings("unchecked") T[] arr = (T[]) Array.newInstance(clazz, length);
+        Node<T> current = head;
+        int counter = 0;
+        while (current != null) {
+            arr[counter++] = current.getValue();
+            current = current.next;
+        }
+        return arr;
     }
 }
--- src/DataStructure/LinkedList/driver.java
+++ src/DataStructure/LinkedList/driver.java
@@ -2,24 +2,93 @@ package src.DataStructure.LinkedList;
 
 public class driver {
     public static void main(String[] args) {
-        LinkList<Integer> test = new LinkList<>();
+        LinkList<Integer> test = new LinkList<>(Integer.class);
+        System.out.println("******** Integer test ********");
+        System.out.println("=== Add Test ===");
+        test.add(0);
         test.add(1);
         test.add(2);
         test.add(3);
         test.add(4);
-        test.add(5);
+        System.out.println("LinkList constructed successfully");
+        test.print(); // 0, 1, 2, 3, 4
         System.out.println("Length = " + test.getLength());
-        test.print();
-        System.out.println("Index of 5 = ");
-        System.out.println("Deleted 3: " + test.delete(3));
+        System.out.println("Index of 4 = " + test.indexOf(4));
+        System.out.println("\nDeleted 3: " + test.delete(3));
+        test.print(); // 0, 1, 2, 3,
         System.out.println("Length = " + test.getLength());
-        test.print();
-        System.out.println("Deleted 5: " + test.delete(5));
-        System.out.println("Length = " + test.getLength());
-        test.print();
-        System.out.println("Deleted 1: " + test.delete(1));
-        System.out.println("Length = " + test.getLength());
-        test.print();
+        System.out.println("Index of 4 = " + test.indexOf(4));
 
+        System.out.println("\nDeleted 5: " + test.delete(5));
+        test.print();
+        System.out.println("Length = " + test.getLength());
+        System.out.println("Index of 5 = " + test.indexOf(5));
+
+
+        System.out.println("\nDeleted 1: " + test.delete(1));
+        test.print();
+        System.out.println("Length = " + test.getLength());
+        System.out.println("Index of 1 = " + test.indexOf(1));
+
+        System.out.println("\nAdd: 0 , 1, 2, 3, 4");
+        test.add(0);
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        test.add(4);
+        test.print();
+        System.out.println("\nPrint index pairs ...");
+        test.printIndex();
+        System.out.println("\nInsert: index = 4, value = 200");
+        test.insert(4, 200);
+        test.printIndex();
+        System.out.println("\nRemove: index = 4");
+        test.removeAt(4);
+        test.printIndex();
+
+        LinkList<String> list = new LinkList<>(String.class);
+        System.out.println("\n\n******** String test ********");
+        System.out.println("=== Add Test ===");
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        list.print();            // {Apple, Banana, Cherry, }
+        list.printIndex();       // [(0): Apple, (1): Banana, (2): Cherry, ]
+
+        System.out.println("\n=== Insert Test ===");
+        list.insert(1, "Blueberry");
+        list.print();            // {Apple, Blueberry, Banana, Cherry, }
+        list.printIndex();       // [(0): Apple, (1): Blueberry, (2): Banana, (3): Cherry, ]
+
+        System.out.println("\n=== Get Test ===");
+        System.out.println("Element at index 2: " + list.get(2)); // Banana
+
+        System.out.println("\n=== IndexOf Test ===");
+        System.out.println("Index of Cherry: " + list.indexOf("Cherry")); // 3
+        System.out.println("Index of Mango: " + list.indexOf("Mango"));   // -1
+
+        System.out.println("\n=== Delete Test ===");
+        list.print();
+        System.out.println("Delete: 'Banana'");
+        list.delete("Banana");
+        list.print();            // {Apple, Blueberry, Cherry, }
+        list.printIndex();       // [(0): Apple, (1): Blueberry, (2): Cherry, ]
+
+        System.out.println("\n=== RemoveAt Test ===");
+        list.print();
+        System.out.println("Remove: index 1");
+        list.removeAt(1);        // remove "Blueberry"
+        list.print();            // {Apple, Cherry, }
+        list.printIndex();       // [(0): Apple, (1): Cherry, ]
+
+        System.out.println("\n=== Insert at Head and Tail Test ===");
+        list.insert(0, "First");
+        list.insert(list.getLength(), "Last");
+        list.print();            // {First, Apple, Cherry, Last, }
+        list.printIndex();       // [(0): First, (1): Apple, (2): Cherry, (3): Last, ]
+
+        System.out.println("\n=== toArray Test ===");
+        String[] arr = list.toArray();
+        System.out.println("Array: " + java.util.Arrays.toString(arr));  // [First, Apple, Cherry, Last]
     }
 }
