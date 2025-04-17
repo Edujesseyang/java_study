2edaf6de5b9f8d4c73e872dbf82e46155b595acd
Edujesseyang <edujesseyang@gmail.com>
2025-04-17 13:35:51 -0700

    add
    add

--- src/DataStructure/ArrayList/MyArrayList.java
+++ src/DataStructure/ArrayList/MyArrayList.java
@@ -2,11 +2,38 @@ package src.DataStructure.ArrayList;
 
 public class MyArrayList<T> {
     private T[] baseArr;
-    private int size = 20;
+    private int size;
+    private int length;
 
-    public MyArrayList(){
-        T[] = new T[size];
+    @SuppressWarnings("unchecked")
+    public MyArrayList(Class<T> clazz) {
+        this.size = 20;
+        this.length = 0;
+        baseArr = (T[]) java.lang.reflect.Array.newInstance(clazz, size);
     }
 
+    public int length() {
+        return this.length;
+    }
+
+    public void add(T val) {
+
+    }
+
+    public boolean contains(T val) {
+
+
+        return false;
+    }
+
+    public boolean delete(T val) {
+
+        return false;
+    }
+
+    public T elementAt(int index){
+
+
+    }
 
 }
