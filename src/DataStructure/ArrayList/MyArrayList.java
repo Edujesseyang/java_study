0acbbbb694b3f64753b19f8404e2261a36b36e3a
Edujesseyang <edujesseyang@gmail.com>
2025-04-17 13:26:18 -0700

    add
    add

---
+++ src/DataStructure/ArrayList/MyArrayList.java
@@ -0,0 +1,12 @@
+package src.DataStructure.ArrayList;
+
+public class MyArrayList<T> {
+    private T[] baseArr;
+    private int size = 20;
+
+    public MyArrayList(){
+        T[] = new T[size];
+    }
+
+
+}
